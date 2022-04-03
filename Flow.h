cbda7c0ff10443a85171a1c84bd74b880578a90d
Roger Puig <puig.font.roger@alumnat.copernic.cat>
2022-04-03 21:49:04 +0200

    Flow Object created

---
+++ Flow.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Flow {
+public:
+	string type;
+	string time;
+	string depa;
+	string dest;
+	string validDate;
+	string validTime;
+
+	Flow(string mytype, string mytime, string mydepa, string mydest, string myvalidDate, string myvalidTime) : 
+		type(mytype), time(mytime), depa(mydepa), dest(mydest), validDate(myvalidDate), validTime(myvalidTime) { }
+};
