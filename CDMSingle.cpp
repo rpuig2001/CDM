c08b8a2cbb74d2f26a1e08886b5b93a99ab673a6
rpuig11 <rpuig2001@gmail.com>
2022-09-22 17:24:14 +0200

    fix  #63

--- CDMSingle.cpp
+++ CDMSingle.cpp
@@ -480,6 +480,37 @@ void CDM::OnFunctionCall(int FunctionId, const char* ItemString, POINT Pt, RECT 
 				}
 
 				fp.GetControllerAssignedData().SetFlightStripAnnotation(1, (hour + min).c_str());
+				
+				//Reload CTOT
+				for (int a = 0; a < slotList.size(); a++)
+				{
+					if (slotList[a].callsign == fp.GetCallsign()) {
+						if (slotList[a].hasCtot) {
+							slotList.erase(slotList.begin() + a);
+						}
+					}
+				}
+
+				for (int i = 0; i < slotList.size(); i++)
+				{
+					if (slotList[i].callsign == fp.GetCallsign()) {
+						slotList[i].hasCtot = false;
+						slotList[i].ctot = "";
+						string remarks = fp.GetControllerAssignedData().GetFlightStripAnnotation(3);
+						if (remarks.find("CTOT") != string::npos) {
+							if (remarks.find("%") != string::npos) {
+								string stringToAdd = remarks.substr(0, remarks.find("CTOT")) + remarks.substr(remarks.find("%"), remarks.length() - remarks.find("%"));
+								fp.GetControllerAssignedData().SetFlightStripAnnotation(3, stringToAdd.c_str());
+								remarks = stringToAdd;
+							}
+							else {
+								string stringToAdd = remarks.substr(0, remarks.find("CTOT") - 1);
+								fp.GetControllerAssignedData().SetFlightStripAnnotation(3, stringToAdd.c_str());
+								remarks = stringToAdd;
+							}
+						}
+					}
+				}
 			}
 			else {
 				fp.GetControllerAssignedData().SetFlightStripAnnotation(1, "");
