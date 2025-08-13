d180a3b19e0bf8451d0ad1af262bdf5687623557
Roger Puig <68125167+rpuig2001@users.noreply.github.com>
2025-08-13 11:12:42 +0200

    2.2.8.9

--- CDMSingle.hpp
+++ CDMSingle.hpp
@@ -26,7 +26,7 @@
 #pragma comment(lib, "Wininet")
 
 #define MY_PLUGIN_NAME      "CDM Plugin"
-#define MY_PLUGIN_VERSION   "2.2.8.8"
+#define MY_PLUGIN_VERSION   "2.2.8.9"
 #define MY_PLUGIN_DEVELOPER "Roger Puig"
 #define MY_PLUGIN_COPYRIGHT "GPL v3"
 #define MY_PLUGIN_VIEW_AVISO  "Euroscope CDM"
